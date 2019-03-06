2fac1ae224596f4cbc86bdec9f5e008d9e9dcb5b
necheporenko <ne4eporenko.v@gmail.com>
2019-03-06 16:10:55 +0200

    feat: about

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const About = () => (
-  <h2 className="empty">
-    About <br /> Empty Page
-  </h2>
-);
+import { Row } from 'antd';
+import { ReactComponent as IconAbout } from '../../components/Icons/about.svg';
+
+class About extends Component {
+  state = {
+    message: ''
+  };
+
+  handleChange = event => {
+    this.setState({ message: event.target.value });
+  };
+
+  sendForm = () => {
+    setTimeout(() => {
+      this.setState({ message: '' });
+    }, 2000);
+  };
+
+  render() {
+    const { message } = this.state;
+
+    return (
+      <div className="about">
+        <Row className="about__content" type="flex" justify="center" align="middle">
+          <IconAbout />
+          <div className="about__content__description">The test uses the typology of</div>
+          <div className="about__content__name">Carl Gustav Jung</div>
+          <div className="about__content__label">We welcome any comments and suggestions:</div>
+          <textarea
+            id="about__message"
+            name="about__message"
+            className="textarea"
+            placeholder="Write your answer"
+            value={message}
+            onChange={this.handleChange}
+          />
+          <button onClick={() => this.sendForm()} className="btn">
+            SEND
+          </button>
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default About;
