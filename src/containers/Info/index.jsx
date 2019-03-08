57f19a019615fa0c848b487f8a9442a6417843e7
necheporenko <ne4eporenko.v@gmail.com>
2019-03-08 20:19:57 +0200

    feat: add pdf

--- src/containers/Info/index.jsx
+++ src/containers/Info/index.jsx
@@ -1,128 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Row } from 'antd';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 
 import Icon from '../../components/Icons';
+import PDF from '../../components/PDF';
 
-import PDF from './PDF_report.pdf';
+import { ANSWER_ARRAY } from '../../contstants';
 
-const ANSWER_ARRAY = [
-  {
-    icon: 'wind',
-    iconXs: 'xsWind',
-    abbreviation: 'INTJ',
-    title: 'Introverted Intuitive Thinking Judging',
-    type: 'Architect'
-  },
-  {
-    icon: 'wind',
-    iconXs: 'xsFire',
-    abbreviation: 'ESFJ',
-    title: 'Extraverted Sensing Feeling Judging',
-    type: 'Consul'
-  },
-  {
-    icon: 'wind',
-    iconXs: 'xsWater',
-    abbreviation: 'ENTP',
-    title: 'Extraverted Intuitive Thinking Perceiving',
-    type: 'Logician'
-  },
-  {
-    icon: 'wind',
-    iconXs: 'xsEarth',
-    abbreviation: 'ISFP',
-    title: 'Introverted Sensing Feeling Perceiving',
-    type: 'Adventurer'
-  },
-
-  {
-    icon: 'fire',
-    iconXs: 'xsWind',
-    abbreviation: 'INFP',
-    title: 'Introverted Intuitive Feeling Perceiving',
-    type: 'Mediator'
-  },
-  {
-    icon: 'fire',
-    iconXs: 'xsFire',
-    abbreviation: 'ISTJ',
-    title: 'Extraverted Sensing Thinking Perceiving',
-    type: 'Entrepreneur'
-  },
-  {
-    icon: 'fire',
-    iconXs: 'xsWater',
-    abbreviation: 'ENFJ',
-    title: 'Extraverted Intuitive Feeling Judging',
-    type: 'Protagonist'
-  },
-  {
-    icon: 'fire',
-    iconXs: 'xsEarth',
-    abbreviation: 'ISTJ',
-    title: 'Introverted Sensing Thinking Judging',
-    type: 'Logistician'
-  },
-
-  {
-    icon: 'water',
-    iconXs: 'xsWind',
-    abbreviation: 'INTP',
-    title: 'Introverted Intuitive Thinking Perceiving',
-    type: 'Logician'
-  },
-  {
-    icon: 'water',
-    iconXs: 'xsFire',
-    abbreviation: 'ESFP',
-    title: 'Extraverted Sensing Feeling Perceiving',
-    type: 'Entertainer'
-  },
-  {
-    icon: 'water',
-    iconXs: 'xsWater',
-    abbreviation: 'ENTJ',
-    title: 'Extraverted Intuitive Thinking Judging',
-    type: 'Commander'
-  },
-  {
-    icon: 'water',
-    iconXs: 'xsEarth',
-    abbreviation: 'ISFJ',
-    title: 'Introverted Sensing Feeling Judging',
-    type: 'Defender'
-  },
-
-  {
-    icon: 'earth',
-    iconXs: 'xsWind',
-    abbreviation: 'INFJ',
-    title: 'Introverted Intuitive Feeling Judging',
-    type: 'Advocate'
-  },
-  {
-    icon: 'earth',
-    iconXs: 'xsFire',
-    abbreviation: 'ESTJ',
-    title: 'Extraverted Sensing Thinking Judging',
-    type: 'Executive'
-  },
-  {
-    icon: 'earth',
-    iconXs: 'xsWater',
-    abbreviation: 'ENFP',
-    title: 'Extraverted Intuitive Feeling Perceiving',
-    type: 'Campaigner'
-  },
-  {
-    icon: 'earth',
-    iconXs: 'xsEarth',
-    abbreviation: 'ISTP',
-    title: 'Introverted Sensing Thinking Perceiving',
-    type: 'Virtuoso'
-  }
-];
+// import PDF from './PDF_report.pdf';
 
 class Info extends Component {
   state = {
@@ -167,11 +53,7 @@ class Info extends Component {
           <div className="element-icon__wrapper">
             <Icon name={icon} finished={true} active={true} />
             <div className="small-icon">
-              <Icon
-                name={iconXs}
-                // finished={test.status === 'finished' || false}
-                active={true}
-              />
+              <Icon name={iconXs} active={true} />
             </div>
           </div>
           <div className="info__content__title-abbreviation">{abbreviation}</div>
@@ -181,9 +63,20 @@ class Info extends Component {
             According to the test results with a probability of {this.getRandomNumber(80, 95)}%.
             Your personality type is {type}.
           </div>
-          <a href={PDF} download="file.pdf">
+          {/* <a href={PDF} download="file.pdf">
             <button className="btn">Download PDF report</button>
-          </a>
+          </a> */}
+
+          <PDFDownloadLink document={<PDF title="Hello PDF" />} fileName="result.pdf">
+            {({ blob, url, loading, error }) =>
+              loading ? (
+                <button className="btn">Creating PDF report...</button>
+              ) : (
+                <button className="btn">Download PDF report</button>
+              )
+            }
+          </PDFDownloadLink>
+
           <div className="info__content__probability">
             The probability of other types according to the test:
           </div>
--- src/contstants/index.js
+++ src/contstants/index.js
@@ -15,3 +15,121 @@ export const getAnswerLabel = smile => {
       return null;
   }
 };
+
+export const ANSWER_ARRAY = [
+  {
+    icon: 'wind',
+    iconXs: 'xsWind',
+    abbreviation: 'INTJ',
+    title: 'Introverted Intuitive Thinking Judging',
+    type: 'Architect'
+  },
+  {
+    icon: 'wind',
+    iconXs: 'xsFire',
+    abbreviation: 'ESFJ',
+    title: 'Extraverted Sensing Feeling Judging',
+    type: 'Consul'
+  },
+  {
+    icon: 'wind',
+    iconXs: 'xsWater',
+    abbreviation: 'ENTP',
+    title: 'Extraverted Intuitive Thinking Perceiving',
+    type: 'Logician'
+  },
+  {
+    icon: 'wind',
+    iconXs: 'xsEarth',
+    abbreviation: 'ISFP',
+    title: 'Introverted Sensing Feeling Perceiving',
+    type: 'Adventurer'
+  },
+
+  {
+    icon: 'fire',
+    iconXs: 'xsWind',
+    abbreviation: 'INFP',
+    title: 'Introverted Intuitive Feeling Perceiving',
+    type: 'Mediator'
+  },
+  {
+    icon: 'fire',
+    iconXs: 'xsFire',
+    abbreviation: 'ISTJ',
+    title: 'Extraverted Sensing Thinking Perceiving',
+    type: 'Entrepreneur'
+  },
+  {
+    icon: 'fire',
+    iconXs: 'xsWater',
+    abbreviation: 'ENFJ',
+    title: 'Extraverted Intuitive Feeling Judging',
+    type: 'Protagonist'
+  },
+  {
+    icon: 'fire',
+    iconXs: 'xsEarth',
+    abbreviation: 'ISTJ',
+    title: 'Introverted Sensing Thinking Judging',
+    type: 'Logistician'
+  },
+
+  {
+    icon: 'water',
+    iconXs: 'xsWind',
+    abbreviation: 'INTP',
+    title: 'Introverted Intuitive Thinking Perceiving',
+    type: 'Logician'
+  },
+  {
+    icon: 'water',
+    iconXs: 'xsFire',
+    abbreviation: 'ESFP',
+    title: 'Extraverted Sensing Feeling Perceiving',
+    type: 'Entertainer'
+  },
+  {
+    icon: 'water',
+    iconXs: 'xsWater',
+    abbreviation: 'ENTJ',
+    title: 'Extraverted Intuitive Thinking Judging',
+    type: 'Commander'
+  },
+  {
+    icon: 'water',
+    iconXs: 'xsEarth',
+    abbreviation: 'ISFJ',
+    title: 'Introverted Sensing Feeling Judging',
+    type: 'Defender'
+  },
+
+  {
+    icon: 'earth',
+    iconXs: 'xsWind',
+    abbreviation: 'INFJ',
+    title: 'Introverted Intuitive Feeling Judging',
+    type: 'Advocate'
+  },
+  {
+    icon: 'earth',
+    iconXs: 'xsFire',
+    abbreviation: 'ESTJ',
+    title: 'Extraverted Sensing Thinking Judging',
+    type: 'Executive'
+  },
+  {
+    icon: 'earth',
+    iconXs: 'xsWater',
+    abbreviation: 'ENFP',
+    title: 'Extraverted Intuitive Feeling Perceiving',
+    type: 'Campaigner'
+  },
+  {
+    icon: 'earth',
+    iconXs: 'xsEarth',
+    abbreviation: 'ISTP',
+    title: 'Introverted Sensing Thinking Perceiving',
+    type: 'Virtuoso'
+  }
+];
